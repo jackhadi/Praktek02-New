f4b82638d24f0b9c081a6282ea5f8f3ea864e9c8
jackhadi <jackhadi@gmail.com>
2018-04-26 23:24:58 -0700

    mengisi nilai attribut

--- src/praktek02/pkgnew/RectangleAksi.java
+++ src/praktek02/pkgnew/RectangleAksi.java
@@ -4,6 +4,8 @@ package praktek02.pkgnew;
 public class RectangleAksi {
         public static void main(String[] args) {
         Rectangle a1 = new Rectangle();
+        a1.panjang = 10;
+        a1.lebar = 30;
 
     }
 }
